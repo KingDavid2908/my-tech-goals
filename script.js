40f332bb49c4a072a6e2d50c91e3490171587133
Chukwumam Akachukwu David <akachukwumam1@gmail.com>
2024-07-01 13:17:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function updateTime() {
+    const now = new Date();
+    const utcTime = now.toUTCString().split(" ")[4];
+    const dayOfWeek = now.toLocaleString("en-us", {
+      weekday: "long",
+    });
+
+    document.getElementById("current-time").textContent = utcTime;
+    document.getElementById("current-day").textContent = dayOfWeek;
+  }
+
+  updateTime();
+  setInterval(updateTime, 1000);
+});
